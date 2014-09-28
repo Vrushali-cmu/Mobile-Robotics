f9b973cbe2c003f50229f04d36d56d4b5eca9993
Siddhant Wadhwa <Siddhant.Wadhwa@gmail.com>
2014-09-28 12:29:25 -0400

    Lab5 warmup 1

---
+++ Lab5/robotModel.m
@@ -0,0 +1,25 @@
+classdef robotModel < handle
+    properties
+        W = 0.235; %width of the robot (in m)
+        W2 = 0.1175;  %width/2 of the robot (in m)
+    end
+    methods(Static)
+        function [vl,vr] = VwTovlvr(V,w)
+            vl = V-W2*w;   %left wheel velocity
+            vr = V+W2*w;   %right wheel velocity
+            
+            %saturate absolute velocities at 0.3 m/s
+            if abs(vl)>0.3
+               vl = 0.3*vl/abs(vl);        
+            end
+            if abs(vr)>0.3
+               vr = 0.3*vr/abs(vr);
+            end
+            
+        end
+        function [V w] = vlvrToVw(vl,vr)
+            V = (vl+vr)/2;  %velocity
+            w = (vr-vl)/W;  %angular velocity
+        end
+    end
+end
